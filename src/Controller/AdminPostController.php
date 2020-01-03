01abb799952c3a9adb05cc343a2a2e27b938c467
anonbnr <bashar.nabil.rima@gmail.com>
2020-01-03 18:54:00 +0100

    [Authentication] put in place FOSUserBundle infrastructure and tested without explicit role assignment

--- src/Controller/AdminPostController.php
+++ src/Controller/AdminPostController.php
@@ -10,9 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-// /**
-//  * @Route("/admin/post")
-//  */
+/**
+ * @Route("/admin/post")
+ */
 class AdminPostController extends AbstractController
 {
   /**
